6fad2f52e5e687802a4c4e5a3cae37a254872104
William Arends <willarends@gmail.com>
2018-03-14 17:48:25 -0700

    post details refinement

--- public/app/posts/post-detail.controller.js
+++ public/app/posts/post-detail.controller.js
@@ -1,11 +1,11 @@
 angular.module('post-detail.controller', []).controller('PostDetailController', ['$scope', '$stateParams', '$http', 'Meta', 'NotifierService', function($scope, $stateParams, $http, Meta, notifier){
 
-    Meta.setTitle('Post Detail');
     $scope.post;
     const slug = $stateParams.slug;
 
     $http.get(`/api/post/${slug}`).then((res)=> {
         $scope.post = res.data;
+        Meta.setTitle($scope.post.title);
         console.log(res.data);
     });
 
