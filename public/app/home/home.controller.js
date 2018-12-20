696804163af9fb3d778d9b5686834fa2b035f5ed
warends <willarends@gmail.com>
2018-12-20 15:14:46 -0800

    set home page social image

--- public/app/home/home.controller.js
+++ public/app/home/home.controller.js
@@ -6,6 +6,9 @@ angular.module('home.controller', []).controller('HomeController', ['$scope', '$
 		
 		$http.get('/api/home/content').then((res)=> {
 			$scope.content = res.data[0];
+			if ($scope.content.seoImage) {
+				Meta.setSocialImage($scope.content.seoImage.secure_url);
+			}
 		});
 
     $scope.screenW = $window.innerWidth;
