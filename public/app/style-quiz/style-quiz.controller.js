69ccc07604ae84fbe64aa892388cf387370d3a64
warends <willarends@gmail.com>
2018-12-20 15:33:25 -0800

    set style quiz

--- public/app/style-quiz/style-quiz.controller.js
+++ public/app/style-quiz/style-quiz.controller.js
@@ -1,7 +1,7 @@
 angular.module('style-quiz.controller', []).controller('StyleQuizController', ['$scope', '$http', 'StyleQuizFactory', 'NotifierService', 'Meta', ($scope, $http, StyleQuizFactory, notifier, Meta) => {
 
     Meta.setTitle('Style Quiz | Cedar + Sage Design | Online Interior Design Studio');
-    Meta.setSocialImage('https://www.cedarandsagedesign.com/img/style-quiz/style-quiz-seo.jpg');
+    Meta.setSocialImage('/img/style-quiz/style-quiz-seo.jpg');
 
 	window.scrollTo(0, 0);
 
