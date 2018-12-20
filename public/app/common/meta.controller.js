7152f4b153415935165032fd8eeb68cd3fd26548
warends <willarends@gmail.com>
2018-12-20 15:35:21 -0800

    one more image

--- public/app/common/meta.controller.js
+++ public/app/common/meta.controller.js
@@ -2,7 +2,7 @@ angular.module('common.meta', []).factory('Meta', function(){
   let title = 'Cedar + Sage Online Interior Design Studio';
   let desc = 'Where DIY meets professional interior design. We offer online interior design, space planning, branding, residential and furniture design.';
 	let keywords = 'boutique interior design studio, personally tailored services, virtual interior design, online interior design, interior design for everyone, shop your purchasing list, design style, style quiz, captivating compositions, decorate your home online';
-	let socialImage = 'http://www.cedarandsagedesign.com/img/curator/moodboard.png';
+	let socialImage = 'https://www.cedarandsagedesign.com/img/curator/moodboard.png';
 
   return {
     title: () => title,
