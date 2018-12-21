b76a7a7f31e1acd9efc85e0776ce8e08414af126
warends <willarends@gmail.com>
2018-12-20 19:34:08 -0800

    post seo image

--- public/app/posts/post-detail.controller.js
+++ public/app/posts/post-detail.controller.js
@@ -1,11 +1,13 @@
-angular.module('post-detail.controller', []).controller('PostDetailController', ['$scope', '$stateParams', '$http', 'Meta', 'NotifierService', function($scope, $stateParams, $http, Meta, notifier){
+angular.module('post-detail.controller', []).controller('PostDetailController', ['$scope', '$stateParams', '$http', 'Meta', function($scope, $stateParams, $http, Meta){
 
     $scope.post;
     const slug = $stateParams.slug;
 
     $http.get(`/api/post/${slug}`).then((res)=> {
         $scope.post = res.data;
-        Meta.setTitle($scope.post.title);
+		Meta.setTitle($scope.post.title);
+		Meta.setSocialImage($scope.post.image.url);
+		Meta.setSecureSocialImage($scope.post.image.secure_url);
     });
 
 }]);
