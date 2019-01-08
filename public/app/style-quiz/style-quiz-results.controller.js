0bb05ab187d85c48a9eeeb801dda9e874274baab
Will Arends <willarends@gmail.com>
2019-01-08 13:07:39 -0800

    pintrest button

--- public/app/style-quiz/style-quiz-results.controller.js
+++ public/app/style-quiz/style-quiz-results.controller.js
@@ -1,6 +1,5 @@
 angular.module('style-quiz-results.controller', []).controller('StyleQuizResultsController', ['$scope', '$state', '$location', 'StyleQuizFactory', 'Meta', ($scope, $state, $location, StyleQuizFactory, Meta) => {
 
-	Meta.setTitle('Style Quiz | Cedar + Sage Design | Online Interior Design Studio');
 	Meta.setSocialImage('http://res.cloudinary.com/www-cedarandsage-com/image/upload/v1545351821/style-quiz-seo.jpg');
 	Meta.setSecureSocialImage('https://res.cloudinary.com/www-cedarandsage-com/image/upload/v1545351821/style-quiz-seo.jpg');
 
@@ -19,8 +18,11 @@ angular.module('style-quiz-results.controller', []).controller('StyleQuizResults
 		}
 	});
 
+	Meta.setTitle(`Your style is ${$scope.formattedAnswers.toUpperCase()} | Style Quiz | Cedar + Sage Design | Online Interior Design Studio`);
+
 	$scope.shareFB = () => {
 		const absUrl = $location.absUrl();
+		console.log(absUrl);
 		window.open("https://www.facebook.com/sharer/sharer.php?u="+escape(absUrl)+"&t="+document.title, '', 
 		'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');
 		return false;
