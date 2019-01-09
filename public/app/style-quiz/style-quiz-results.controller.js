8686cf79fb06a2ad6df82f5177c1b0874c4c93d9
Will Arends <willarends@gmail.com>
2019-01-08 21:08:19 -0800

    update link to share

--- public/app/style-quiz/style-quiz-results.controller.js
+++ public/app/style-quiz/style-quiz-results.controller.js
@@ -23,9 +23,8 @@ angular.module('style-quiz-results.controller', []).controller('StyleQuizResults
 		}
 	});
 
-	$scope.absUrl = $location.absUrl();
-	$scope.encodedUrl = encodeURIComponent($scope.absUrl);
-	const fbUrl = `https://www.facebook.com/plugins/share_button.php?href=${$scope.encodedUrl}&layout=button&size=large&mobile_iframe=false&appId=1246936782112521&width=73&height=28`;
+	$scope.encodedUrl = encodeURIComponent(`https://www.cedarandsagedesign.com/style-quiz`);
+	const fbUrl = `https://www.facebook.com/plugins/share_button.php?href=${$scope.encodedUrl}&layout=button&size=large&mobile_iframe=false&quote=My%20style%20is%20${formattedAnswers.toUpperCase()}%20|%20Style%20Quiz%20Cedar%20+%20Sage%20Design&appId=1246936782112521&width=73&height=28`;
 	$scope.fbShare = $sce.trustAsResourceUrl(fbUrl);
 
 	Meta.setTitle(`Your style is ${$scope.formattedAnswers.toUpperCase()} | Style Quiz | Cedar + Sage Design | Online Interior Design Studio`);
