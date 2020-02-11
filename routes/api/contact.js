2bfeaac62bec812ae775765feefa92a3e78e1fd6
warends <willarends@gmail.com>
2020-02-11 12:40:25 -0800

    updated mailgun creds and templates

--- routes/api/contact.js
+++ routes/api/contact.js
@@ -1,105 +1,133 @@
-const nodemailer = require('nodemailer');
-console.log('sending email as ' + process.env.GMAIL_HB);
+const Email = require('keystone-email');
+const key = process.env.MAILGUN_API_KEY;
+const domain = process.env.MAILGUN_DOMAIN;
 
-//create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-    host: 'smtpout.secureserver.net',
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.GMAIL_HB,
-        pass: process.env.GMAIL_HB_PW
-    }
-});
-
-transporter.verify(function(error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take our messages');
-  }
-});
-
-exports.contactForm = function(req, res) {
-    var data = req.body;
-    var mailOptions = {
-      from: 'hayley@cedarandsagedesign.com',
-      to: 'haybagwell@gmail.com',
-      subject: 'Message from ' + data.name,
-      html: 'Message: ' + data.note + '<br><br><p>Email: ' + data.email + '<br>Name: ' + data.name + '</p>'
-    }
-    transporter.sendMail(mailOptions, function(err, info){
-      if(err){
-        console.log(err);
-        res.json({message: err.toString()});
-      } else {
-        res.json(data);
-      }
-    });
-}
-
-exports.qForm = function(req, res) {
-    var data = req.body;
-    var mailOptions = {
-      from: 'hayley@cedarandsagedesign.com',
-      to: 'haybagwell@gmail.com',
-      subject: 'A customer filled out your questioniare!',
-      html: `Room to decorate first: ${data.q1}
-            <br><br>What images are you drawn to?:  ${data.q2}
-            <br><br>Where do you shop for furniture?: ${data.q3}
-            <br><br>How much money are you budgeting?:  ${data.q4}
-            <br><br>What colors would you like?:  ${data.q5}
-            <br><br>Where colors do you NOT like?: ${data.q6}
-            <br><br>Name: ${data.name}
-            <br>Phone Number: ${data.phone}
-            <br>Email: ${data.email}
-            <br>Message: ${data.note}`
-    }
-    transporter.sendMail(mailOptions, function(err, info){
-      if(err){
-        console.log(err);
-        res.json({message: err.toString()});
-      } else {
-        res.json(data);
-      }
-    });
-}
-
-exports.styleQuiz = function(req, res) {
-	var data = req.body;
-	var mailOptions = {
-		from: 'hayley@cedarandsagedesign.com',
+exports.contactForm = function (req, res) {
+	new Email('./templates/emails/contact-email.twig', {
+		transport: 'mailgun',
+	}).send({
+		body: {
+			name: req.body.name,
+			note: req.body.note,
+			email: req.body.email,
+		},
+	}, {
+		apiKey: key,
+		domain: domain,
 		to: 'haybagwell@gmail.com',
-		subject: 'A customer filled out your style quiz!',
-		html: `<br><br>Name: ${data.firstName} ${data.lastName}
-					<br>Email: ${data.email}
-					<br>Message: ${data.note}
-					<br>Answers: ${data.answers}`
-	}
-	transporter.sendMail(mailOptions, function(err, info){
-		if(err){
+		from: {
+			name: 'Cedar and Sage Design',
+			email: 'hayley@cedarandsagedesign.com',
+		},
+		subject: `New Message From ${req.body.name}`,
+	}, function (err, info) {
+		if (err) {
 			console.log(err);
-			res.json({message: err.toString()});
+			res.json({ message: err.toString() });
 		} else {
+			console.log('SUCCESS ', info);
+			res.json(req.body);
+		}
+	});
+};
+
+exports.signupForm = function (req, res) {
+	const data = req.body;
+	new Email('./templates/emails/signup-email.twig', {
+		transport: 'mailgun',
+	}).send({
+		body: {
+			firstName: data.firstName,
+			lastName: data.lastName,
+			note: data.note,
+			email: data.email,
+			include: data.include,
+		},
+	}, {
+		apiKey: key,
+		domain: domain,
+		to: 'haybagwell@gmail.com',
+		from: {
+			name: 'Cedar and Sage Design',
+			email: 'hayley@cedarandsagedesign.com',
+		},
+		subject: `${data.firstName} has signed up on Cedar and Sage`,
+	}, function (err, info) {
+		if (err) {
+			console.log(err);
+			res.json({ message: err.toString() });
+		} else {
+			console.log('SUCCESS ', info);
 			res.json(data);
 		}
 	});
-}
+};
 
-exports.signupForm = function(req, res) {
-    var data = req.body;
-    var mailOptions = {
-      from: 'hayley@cedarandsagedesign.com',
-      to: 'haybagwell@gmail.com',
-      subject: `${data.firstName} has signed up on Cedar and Sage`,
-      html: `Email: ${data.email}<br>Name: ${data.firstName} ${data.lastName}<br>Add to email list: ${data.include}`
-    }
-    transporter.sendMail(mailOptions, function(err, info){
-      if(err){
-        console.log(err);
-        res.json({message: err.toString()});
-      } else {
-        res.json(data);
-      }
-    });
-}
+exports.qForm = function (req, res) {
+	const data = req.body;
+	new Email('./templates/emails/q-form-email.twig', {
+		transport: 'mailgun',
+	}).send({
+		body: {
+			name: data.name,
+			phone: data.phone,
+			note: data.note,
+			email: data.email,
+			q1: data.q1,
+			q2: data.q2,
+			q3: data.q3,
+			q4: data.q4,
+			q5: data.q5,
+			q6: data.q6,
+		},
+	}, {
+		apiKey: key,
+		domain: domain,
+		to: 'haybagwell@gmail.com',
+		from: {
+			name: 'Cedar and Sage Design',
+			email: 'hayley@cedarandsagedesign.com',
+		},
+		subject: `A customer filled out your questioniare!`,
+	}, function (err, info) {
+		if (err) {
+			console.log(err);
+			res.json({ message: err.toString() });
+		} else {
+			console.log('SUCCESS ', info);
+			res.json(data);
+		}
+	});
+};
+
+exports.styleQuiz = function (req, res) {
+	const data = req.body;
+	new Email('./templates/emails/style-quiz-email.twig', {
+		transport: 'mailgun',
+	}).send({
+		body: {
+			firstName: data.firstName,
+			lastName: data.lasttName,
+			note: data.note,
+			email: data.email,
+			answers: data.answers,
+		},
+	}, {
+		apiKey: key,
+		domain: domain,
+		to: 'haybagwell@gmail.com',
+		from: {
+			name: 'Cedar and Sage Design',
+			email: 'hayley@cedarandsagedesign.com',
+		},
+		subject: `A customer filled out your style quiz!`,
+	}, function (err, info) {
+		if (err) {
+			console.log(err);
+			res.json({ message: err.toString() });
+		} else {
+			console.log('SUCCESS ', info);
+			res.json(data);
+		}
+	});
+};
