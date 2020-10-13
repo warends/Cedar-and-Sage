7b2926cfb57ba76a900a4a10d177eb3e90306b1e
Will Arends <willarends@gmail.com>
2020-10-13 11:07:32 -0700

    audit packages and gulp

--- public/app/about/about.controller.js
+++ public/app/about/about.controller.js
@@ -1,7 +1,7 @@
 angular.module('about.controller', []).controller('AboutController', ['$scope', '$http', 'Meta', ($scope, $http, Meta) => {
 
-    Meta.setTitle('About your interior designer | Cedar + Sage Design | Online Interior Design Studio');
-    Meta.setDesc(' I’m a neurophysiologist turned interior designer with a rustic eclectic style.  I love to surround myself with soft, organic elements and greenery.');
+	Meta.setTitle('About your interior designer | Cedar + Sage Design | Online Interior Design Studio');
+	Meta.setDesc(' I’m a neurophysiologist turned interior designer with a rustic eclectic style.  I love to surround myself with soft, organic elements and greenery.');
 	Meta.setKeywords('Interior designer, tailoring interior design, Bend, OR, design at any income level, interior design Instagram, passion in design, interior architecture and design, Academy of Arts University');
 	
 	$http.get('/api/about/content').then((res)=> {
